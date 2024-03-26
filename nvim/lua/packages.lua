018d4ae79bd1fad20f1d9b744a99dc7f3b5462c4
Giulio Fratini <giulio.gfratini@gmail.com>
2024-03-26 10:48:33 +0100

    add undotree

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -210,6 +210,14 @@ local packages = {
   },
 
   "RRethy/vim-illuminate",
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
 }
 
 return packages
