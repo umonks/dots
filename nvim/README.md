d24bde49a68a5ea1d3fc095f6ca5e5abae7fd77d
Giulio Fratini <giulio.gfratini@gmail.com>
2025-06-24 15:41:06 +0000

    starting again

--- nvim/README.md
+++
@@ -1,3 +0,0 @@
-# nvimrc
-
-Based on `kickstart.nvim` thanks for your service, you are awesome
--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,35 +1,21 @@
-local keymaps = {}
-
-function keymaps.setup()
-  -- Keymaps for better default experience
-  vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
-  -- Remap for dealing with word wrap
-  vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-  vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
-  -- Diagnostic keymaps
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-  vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-  vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
-  -- Buffers
-  vim.keymap.set('n', '<leader>bd', function() vim.api.nvim_buf_delete(0, {}) end, { desc = 'Delete current buffer' })
-  vim.keymap.set('n', '<leader>sv', function()
-      vim.cmd('vsplit')
-      local win = vim.api.nvim_get_current_win()
-      local buf = vim.api.nvim_create_buf(true, true)
-      vim.api.nvim_win_set_buf(win, buf)
-    end,
-    { desc = 'split current buffer' }
-  )
-
-
-  -- Misc
-  vim.keymap.set('n', '<leader>ee', "oif err != nil {<cr>}<esc>O", { desc = "Check error in go" })
-
-  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+local function nnoremap(binding, cmd, opts)
+	vim.keymap.set('n', binding, cmd, opts)
 end
 
-return keymaps
+nnoremap("gd", vim.lsp.buf.definition)
+nnoremap("gD", vim.lsp.buf.declaration)
+nnoremap("gca", vim.lsp.buf.code_action)
+
+nnoremap("<leader>d", ":bd<CR>")
+
+nnoremap("-", ":Ex<CR>")
+
+local telescope = require("telescope.builtin")
+
+nnoremap("<leader>ff", telescope.find_files)
+nnoremap("<leader>fg", telescope.live_grep)
+nnoremap("<leader>fm", telescope.marks)
+nnoremap("<leader>fw", telescope.lsp_dynamic_workspace_symbols)
+nnoremap("<leader>fd", telescope.diagnostics)
+nnoremap("gr", telescope.lsp_references)
+nnoremap("<leader>rn", vim.lsp.buf.rename)
